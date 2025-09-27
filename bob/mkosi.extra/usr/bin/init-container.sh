5861c7c2663c6c5b514803fbac1f3cefa4c42b8e
alexhulbert <alex@alexhulbert.com>
2025-09-26 21:57:43 -0400

    Working AppArmor

--- bob/mkosi.extra/usr/bin/init-container.sh
+++ bob/mkosi.extra/usr/bin/init-container.sh
@@ -15,7 +15,6 @@ SEARCHER_INPUT_CHANNEL=27017
 echo "Starting $NAME..."
 su -s /bin/sh searcher -c "cd ~ && podman run -d \
     --name $NAME --replace \
-    --security-opt apparmor=searcher-container \
     -p ${SEARCHER_SSH_PORT}:22 \
     -p ${ENGINE_API_PORT}:${ENGINE_API_PORT} \
     -p ${EL_P2P_PORT}:${EL_P2P_PORT} \
