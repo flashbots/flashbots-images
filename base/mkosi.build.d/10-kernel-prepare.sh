8db1ed4add1f0c8b582127b9866add8515ec2934
bakhtin <a@bakhtin.net>
2025-10-31 18:38:46 +0000

    refactor + gcp
    Signed-off-by: bakhtin <a@bakhtin.net>

--- base/mkosi.build.d/10-kernel-prepare.sh
+++ base/mkosi.build.d/10-kernel-prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
 
-apt-get -y build-dep linux-source
-apt-get -y install linux-source --install-recommends
+apt-get -y build-dep linux-source-${KERNEL_VERSION}/trixie-backports
+apt-get -y install linux-source-${KERNEL_VERSION}/trixie-backports --install-recommends
 # socat UNIX-LISTEN:$SRCDIR/debug.sock,fork EXEC:/bin/bash,pty,stderr
