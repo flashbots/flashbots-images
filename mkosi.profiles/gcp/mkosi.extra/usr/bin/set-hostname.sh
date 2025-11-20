e7954c7dc7df97c15555e500f592a6aed3bacfe3
Anton Bronnikov <anton@northernforest.nl>
2025-11-20 09:30:59 +0100

    fix: bail out if curl is not installed

--- mkosi.profiles/gcp/mkosi.extra/usr/bin/set-hostname.sh
+++ mkosi.profiles/gcp/mkosi.extra/usr/bin/set-hostname.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! which curl; then
+  echo "curl is not present on system, exiting..."
+  exit 0
+fi
+
 while true; do
   if hostname=$(
     curl --header "Metadata-Flavor: Google" --silent --show-error \
@@ -10,6 +15,8 @@ while true; do
     echo 127.0.0.1 "${hostname}" >> /etc/hosts
     systemctl restart rsyslog
     exit 0
+  else
+    echo "Failed to query metadata service, will retry in 1s"
   fi
 
   sleep 1
