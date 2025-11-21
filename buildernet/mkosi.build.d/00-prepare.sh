c3cf0422d2f081d3971bb4bf8ad085b003144fc8
bakhtin <a@bakhtin.net>
2025-11-21 16:29:55 +0000

    Multi-images setup
    Signed-off-by: bakhtin <a@bakhtin.net>

--- buildernet/mkosi.build.d/00-prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eu -o pipefail
-
-mkdir -p $SRCDIR/buildernet/mkosi.cache/{go-build,cargo}
---
+++ mkosi.images/buildernet/mkosi.build.d/00-prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+mkdir -p $SRCDIR/mkosi.images/buildernet/mkosi.cache/{go-build,cargo}
