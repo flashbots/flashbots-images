40bd0d140b3652847d5a53a8241fa7e25df8a515
bakhtin <a@bakhtin.net>
2025-11-28 10:33:07 +0000

    rc1
    Signed-off-by: bakhtin <a@bakhtin.net>

--- mkosi.images/buildernet/mkosi.build.d/10-reth.sh
+++ mkosi.images/buildernet/mkosi.build.d/10-reth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -sSfL https://snapshots.flashbots.dev/reth_1.8.2-1_amd64.deb -o $PACKAGEDIR/reth.deb
+curl -sSfL https://snapshots.flashbots.dev/reth_1~1.9.3_amd64.deb -o $PACKAGEDIR/reth.deb
 # TODO
--- mkosi.images/buildernet/mkosi.build.d/11-rbuilder-and-friends.sh
+++ mkosi.images/buildernet/mkosi.build.d/11-rbuilder-and-friends.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-# TODO: prettify
-
 # rbuilder-operator
-EXPECTED_SHA256=a95c8f5039cc525539430112e3c5b3268cd05ea663e717268b28d2d690dcc25e
-curl -sSfL https://github.com/flashbots/rbuilder/releases/download/v1.2.19/rbuilder-operator_1.v1.2.19_amd64.deb -o $PACKAGEDIR/rbuilder-operator.deb
+EXPECTED_SHA256=58f59ad269c8e85f5e4a33aba3bd781bd17a6cba3d6f49abbe6872dbb49d6630
+curl -sSfL https://github.com/flashbots/rbuilder/releases/download/v1.2.29/rbuilder-operator_1.v1.2.29_amd64.deb -o $PACKAGEDIR/rbuilder-operator.deb
 echo "${EXPECTED_SHA256}" $PACKAGEDIR/rbuilder-operator.deb | sha256sum --check
 
 # rbuilder-rebalancer
-EXPECTED_SHA256=cf8f2cf0d3194a94e5d136e66c5cef58038ebe7c391b10c351fe0c0f26c966e9
-curl -sSfL https://github.com/flashbots/rbuilder/releases/download/v1.2.20/rbuilder-rebalancer_1.v1.2.20_amd64.deb -o $PACKAGEDIR/rbuilder-rebalancer.deb
+EXPECTED_SHA256=3e94a9ab5d109ab930d8cbd99f1ce746d7484c8002237d8345511e9c91e87f12
+curl -sSfL https://github.com/flashbots/rbuilder/releases/download/v1.2.29/rbuilder-rebalancer_1.v1.2.29_amd64.deb -o $PACKAGEDIR/rbuilder-rebalancer.deb
 echo "${EXPECTED_SHA256}" $PACKAGEDIR/rbuilder-rebalancer.deb | sha256sum --check
