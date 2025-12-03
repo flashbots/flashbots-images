3c06fc029ab483330da96955e42c9a453e835e5a
alexhulbert <alex@alexhulbert.com>
2025-12-03 21:43:50 +0900

    Use default mirror uri, not empty file

--- base/add-backports.sh
+++ base/add-backports.sh
@@ -5,9 +5,10 @@
 # See https://github.com/systemd/mkosi/issues/1755
 MIRROR=$(jq -r .Mirror /work/config.json)
 if [ "$MIRROR" = "null" ]; then
-    : > "$SRCDIR/mkosi.builddir/debian-backports.sources"
-else
-    cat > "$SRCDIR/mkosi.builddir/debian-backports.sources" <<EOF
+    MIRROR="http://deb.debian.org/debian"
+fi
+
+cat > "$SRCDIR/mkosi.builddir/debian-backports.sources" <<EOF
 Types: deb deb-src
 URIs: $MIRROR
 Suites: ${RELEASE}-backports
@@ -15,4 +16,3 @@ Components: main
 Enabled: yes
 Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 EOF
-fi
