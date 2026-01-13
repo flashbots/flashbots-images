a4b862c08447e7f5f4734ce81fc3ec4006a3a44f
alexhulbert <alex@alexhulbert.com>
2026-01-13 17:47:29 -0500

    Update GCP measurement commit

--- flake.nix
+++ flake.nix
@@ -44,8 +44,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "flashbots";
         repo = "dstack-mr-gcp";
-        rev = "503e7c506f89f9d81be04025c90921778b26f0a4";
-        sha256 = "sha256-z6STTgcOXatiqA2rlpzwRyvAwnXrK30oNDCJqtIp7/8=";
+        rev = "ee95d36c8f18d159f6ada31474555e4a253b3897";
+        sha256 = "sha256-vAYN4zFXHSxd86KP+Toqh1ZDa4+KGLNsQoOuTr45pGg=";
       };
       vendorHash = "sha256-glOyRTrIF/zP78XGV+v58a1Bec6C3Fvc5c8G3PglzPM=";
     };
