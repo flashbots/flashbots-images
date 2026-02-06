1dd1c254ee962f30cb3cb34d8370a3e40f3cff9b
bakhtin <a@bakhtin.net>
2026-02-06 10:26:58 +0000

    reth 1.10.2
    Signed-off-by: bakhtin <a@bakhtin.net>

--- mkosi.images/buildernet/mkosi.build.d/10-reth.sh
+++ mkosi.images/buildernet/mkosi.build.d/10-reth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-EXPECTED_SHA256=a092377ed4249108a8b51f48f882d96dd00090c1b28677266273258f765795c8
-curl -sSfL https://github.com/paradigmxyz/reth/releases/download/v1.10.0/reth-v1.10.0-x86_64-unknown-linux-gnu-reproducible.deb -o $PACKAGEDIR/reth.deb
+EXPECTED_SHA256=3636fb261b9dd05e359b835055cc1b64b54c80b044d2207a18c7640713b7372c
+curl -sSfL https://github.com/paradigmxyz/reth/releases/download/v1.10.2/reth-v1.10.2-x86_64-unknown-linux-gnu-reproducible.deb -o $PACKAGEDIR/reth.deb
 echo "${EXPECTED_SHA256}" $PACKAGEDIR/reth.deb | sha256sum --check
