5418822064064e7a780bcb001e68ab3a2b82daa8
bakhtin <a@bakhtin.net>
2026-02-26 16:11:15 +0000

    Refactor attested-tls-proxy-server unit
    Signed-off-by: bakhtin <a@bakhtin.net>

--- mkosi.images/buildernet/mkosi.extra/etc/acme-le/hooks/post-post-hook.sh
+++ mkosi.images/buildernet/mkosi.extra/etc/acme-le/hooks/post-post-hook.sh
@@ -7,6 +7,10 @@ install -D -m 600 --owner=operator-api --group=operator-api \
   "$PRIV_KEY" /var/lib/persistent/operator-api/key.pem
 ln -fsr "$(dirname $CERT_PATH)/fullchain.cer" /var/lib/persistent/operator-api/cert.pem
 
+install -D -m 600 --owner=attested-tls-proxy-server --group=attested-tls-proxy-server \
+  "$PRIV_KEY" /var/lib/persistent/attested-tls-proxy-server/key.pem
+ln -fsr "$(dirname $CERT_PATH)/fullchain.cer" /var/lib/persistent/attested-tls-proxy-server/cert.pem
+
 chmod 660 /var/lib/persistent/haproxy/certs/*.pem
 chown haproxy:haproxy /var/lib/persistent/haproxy/certs/*.pem
 systemctl reload haproxy.service
