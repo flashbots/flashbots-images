170b4892349c24e29fb99f3a2ce7be3a77eeb4d6
bakhtin <a@bakhtin.net>
2026-03-09 15:15:56 +0000

    Bump Lighthouse to 8.1.2, reth to v1.11.1
    Signed-off-by: bakhtin <a@bakhtin.net>

--- mkosi.images/buildernet/mkosi.build.d/10-reth.sh
+++ mkosi.images/buildernet/mkosi.build.d/10-reth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-EXPECTED_SHA256=3636fb261b9dd05e359b835055cc1b64b54c80b044d2207a18c7640713b7372c
-curl -sSfL https://github.com/paradigmxyz/reth/releases/download/v1.10.2/reth-v1.10.2-x86_64-unknown-linux-gnu-reproducible.deb -o $PACKAGEDIR/reth.deb
+EXPECTED_SHA256=05d2358f591856077e5d79789ec1aab9462ebdf630055973ef6e18580aabfaed
+curl -sSfL https://github.com/paradigmxyz/reth/releases/download/v1.11.1/reth-v1.11.1-x86_64-unknown-linux-gnu-reproducible.deb -o $PACKAGEDIR/reth.deb
 echo "${EXPECTED_SHA256}" $PACKAGEDIR/reth.deb | sha256sum --check
