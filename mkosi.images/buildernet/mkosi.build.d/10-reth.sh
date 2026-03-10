ae18476c6e47813a3fe90732349bacd35787bd25
bakhtin <a@bakhtin.net>
2026-03-10 16:16:34 +0000

    Bump reth to v1.11.2
    Signed-off-by: bakhtin <a@bakhtin.net>

--- mkosi.images/buildernet/mkosi.build.d/10-reth.sh
+++ mkosi.images/buildernet/mkosi.build.d/10-reth.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-EXPECTED_SHA256=05d2358f591856077e5d79789ec1aab9462ebdf630055973ef6e18580aabfaed
-curl -sSfL https://github.com/paradigmxyz/reth/releases/download/v1.11.1/reth-v1.11.1-x86_64-unknown-linux-gnu-reproducible.deb -o $PACKAGEDIR/reth.deb
+VERSION="v1.11.2"
+EXPECTED_SHA256=66f1460040aabd30bbc4c4b0bfe098da0fb009d7f42fc24e23be5404c5f8f26c
+curl -sSfL https://github.com/paradigmxyz/reth/releases/download/${VERSION}/reth-${VERSION}-x86_64-unknown-linux-gnu-reproducible.deb -o $PACKAGEDIR/reth.deb
 echo "${EXPECTED_SHA256}" $PACKAGEDIR/reth.deb | sha256sum --check
