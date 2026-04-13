978a7f34e03730cdd2b1e98f2b8fe691f1111b64
bakhtin <a@bakhtin.net>
2026-04-13 09:51:33 +0100

    rbuilder v1.3.14 and flowproxy v2.1.2
    Signed-off-by: bakhtin <a@bakhtin.net>

--- mkosi.images/buildernet/mkosi.build.d/11-rbuilder-and-friends.sh
+++ mkosi.images/buildernet/mkosi.build.d/11-rbuilder-and-friends.sh
@@ -3,15 +3,15 @@ set -euo pipefail
 
 echo "Installing rbuilder..."
 
-OPERATOR_VERSION="1.3.13"
-REBALANCER_VERSION="1.3.13"
+OPERATOR_VERSION="1.3.14"
+REBALANCER_VERSION="1.3.14"
 
 # rbuilder-operator
-EXPECTED_SHA256=991b9c47fd5a549edd69f0d5e3524fc71cbbccf586b2250c5c059c03b1e23184
+EXPECTED_SHA256=c91c4cc2fb1ad1b310c6835e3bd10d29fddeae1edc0b0885f9037326768a1e86
 curl -sSfL https://github.com/flashbots/rbuilder/releases/download/v${OPERATOR_VERSION}/rbuilder-operator_1.v${OPERATOR_VERSION}_amd64.deb -o $PACKAGEDIR/rbuilder-operator.deb
 echo "${EXPECTED_SHA256}" $PACKAGEDIR/rbuilder-operator.deb | sha256sum --check
 
 # rbuilder-rebalancer
-EXPECTED_SHA256=9fd7e50d980c7ae8a91e2a1bbe4eed893c0f1f87e4e3de60601a1d74ae4ba79d
+EXPECTED_SHA256=4751e2cc0ec4493f82e29c4b9d146298dc15d3b68ac28ca8db4ba3c94233524d
 curl -sSfL https://github.com/flashbots/rbuilder/releases/download/v${REBALANCER_VERSION}/rbuilder-rebalancer_1.v${REBALANCER_VERSION}_amd64.deb -o $PACKAGEDIR/rbuilder-rebalancer.deb
 echo "${EXPECTED_SHA256}" $PACKAGEDIR/rbuilder-rebalancer.deb | sha256sum --check
