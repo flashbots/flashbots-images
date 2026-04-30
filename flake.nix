f7c2bb6a395888af1ba4c00b986cd427d808bb4c
alexhulbert <alex@alexhulbert.com>
2026-04-30 15:23:37 -0400

    Update to corresponding gcp measure commit

--- flake.nix
+++ flake.nix
@@ -44,53 +44,56 @@
       src = pkgs.fetchFromGitHub {
         owner = "flashbots";
         repo = "dstack-mr-gcp";
-        rev = "503e7c506f89f9d81be04025c90921778b26f0a4";
-        sha256 = "sha256-z6STTgcOXatiqA2rlpzwRyvAwnXrK30oNDCJqtIp7/8=";
+        rev = "ecf3284b72a507fd005de91d49f7372490cf6995";
+        sha256 = "sha256-1rcm9sIZuvCojNN2HMPrsECYn9sd8eVChsgwleo8nFY=";
       };
       vendorHash = "sha256-glOyRTrIF/zP78XGV+v58a1Bec6C3Fvc5c8G3PglzPM=";
     };
     mkosi = system: let
       pkgsForSystem = import nixpkgs {inherit system;};
-      mkosi-unwrapped = (pkgsForSystem.mkosi.override {
-        extraDeps = with pkgsForSystem;
-          [
-            apt
-            dpkg
-            gnupg
-            debootstrap
-            squashfsTools
-            dosfstools
-            e2fsprogs
-            mtools
-            mustache-go
-            cryptsetup
-            gptfdisk
-            util-linux
-            zstd
-            which
-            qemu-utils
-            parted
-            unzip
-            jq
-          ]
-          ++ [reprepro];
-      }).overrideAttrs (old: {
-        src = pkgsForSystem.fetchFromGitHub {
-          owner = "systemd";
-          repo = "mkosi";
-          rev = "df51194bc2d890d4c267af644a1832d2d53339ac";
-          hash = "sha256-rGGzE9xIR8WvK07GBnaAmeLpmnM3Uy51wqyrmuHuWXo=";
-        };
-        # TODO: remove these patch hunks from upstream nixpkgs next time mkosi has a release
-        # The latest mkosi doesn't need them
-        patches = pkgs.lib.drop 2 old.patches;
-        postPatch = let fd = "${pkgs.patchutils}/bin/filterdiff"; in ''
-          { ${fd} -x '*/run.py' --hunks=x2   ${builtins.elemAt old.patches 0}
-            ${fd} -i '*/run.py' --hunks=x1-2 ${builtins.elemAt old.patches 0}
-            ${fd} --hunks=x1                 ${builtins.elemAt old.patches 1}
-          } | patch -p1
-        '';
-      });
+      mkosi-unwrapped =
+        (pkgsForSystem.mkosi.override {
+          extraDeps = with pkgsForSystem;
+            [
+              apt
+              dpkg
+              gnupg
+              debootstrap
+              squashfsTools
+              dosfstools
+              e2fsprogs
+              mtools
+              mustache-go
+              cryptsetup
+              gptfdisk
+              util-linux
+              zstd
+              which
+              qemu-utils
+              parted
+              unzip
+              jq
+            ]
+            ++ [reprepro];
+        }).overrideAttrs (old: {
+          src = pkgsForSystem.fetchFromGitHub {
+            owner = "systemd";
+            repo = "mkosi";
+            rev = "df51194bc2d890d4c267af644a1832d2d53339ac";
+            hash = "sha256-rGGzE9xIR8WvK07GBnaAmeLpmnM3Uy51wqyrmuHuWXo=";
+          };
+          # TODO: remove these patch hunks from upstream nixpkgs next time mkosi has a release
+          # The latest mkosi doesn't need them
+          patches = pkgs.lib.drop 2 old.patches;
+          postPatch = let
+            fd = "${pkgs.patchutils}/bin/filterdiff";
+          in ''
+            { ${fd} -x '*/run.py' --hunks=x2   ${builtins.elemAt old.patches 0}
+              ${fd} -i '*/run.py' --hunks=x1-2 ${builtins.elemAt old.patches 0}
+              ${fd} --hunks=x1                 ${builtins.elemAt old.patches 1}
+            } | patch -p1
+          '';
+        });
     in
       # Create a wrapper script that runs mkosi with unshare
       # Unshare is needed to create files owned by multiple uids/gids
