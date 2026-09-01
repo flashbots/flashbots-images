bffbc11a2ccd6ee2b0c3c2412be188377aaae4b5
alexhulbert <alex@alexhulbert.com>
2026-09-01 16:46:54 -0400

    Add artifact hash to sbom

--- shared/mkosi.postoutput.d/95-gen-sbom.sh
+++ shared/mkosi.postoutput.d/95-gen-sbom.sh
@@ -2,7 +2,12 @@
 set -euo pipefail
 
 root="$OUTPUTDIR/.sbom-root"
-sbom="$OUTPUTDIR/${IMAGE_ID}_${IMAGE_VERSION}.sbom.cdx.json"
+out="$OUTPUTDIR/${IMAGE_ID}_${IMAGE_VERSION}"
+
+# Bind the SBOM to the primary image artifact
+image="$out.efi"
+[[ -f "$out.raw" ]] && image="$out.raw"
+sha=$(sha256sum "$image" | cut -d' ' -f1)
 
 export SYFT_CHECK_FOR_APP_UPDATE=false
 export SYFT_FILE_METADATA_SELECTION=none
@@ -11,7 +16,14 @@ syft scan "dir:$root" \
   --source-name "$IMAGE_ID" \
   --source-version "$IMAGE_VERSION" \
   -o cyclonedx-json |
-  jq 'del(.serialNumber) | del(.components[].properties[] | select(.name == "syft:cpe23")) | .metadata.timestamp = "1970-01-01T00:00:00Z" | .metadata.component += {"bom-ref":"root", "type":"operating-system"}' \
-    >"$sbom"
+  jq --arg file "${image##*/}" --arg sha "$sha" '
+    del(.serialNumber)
+    | del(.components[].properties[]? | select(.name == "syft:cpe23"))
+    | .metadata.timestamp = "1970-01-01T00:00:00Z"
+    | .metadata.component += {
+        "bom-ref": "root", "type": "operating-system",
+        hashes: [{alg: "SHA-256", content: $sha}],
+        properties: [{name: "sbom:image-file", value: $file}]}' \
+    >"$out.sbom.cdx.json"
 
 rm -r "$root"
